5ac8dc0fda5ba57a66bde31125b1b85f93fb6868
tomicervillera <cervilleratomas@outlook.com>
2022-11-29 21:09:05 -0300

    Agregado ejemplo de seeding con faker-js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,28 @@
-const {DataTypes } = require('sequelize');
-const sequelize = require("../database/connection");
-const User = sequelize.define("user", {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-      },
-    name:{
-        type:DataTypes.STRING
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-})
-
-module.exports = User;
+  }
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    userName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    jobTitle: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
